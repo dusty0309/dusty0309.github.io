b2d4ce70131e9d3f2b8682c3751f61f69ab253bf
dusty0309 <isabellalovesacro@gmail.com>
2015-01-23 16:55:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+function choose(choices) {
+    return choices[Math.floor(Math.random() * choices.length)] 
+}
+
+var quotes [
+  'hi',
+  'bye',
+  'hello'
+  ];
+
 var qp = document.querySelector('#quote');
-qp.innerText = 'qqqqqqqqqqqqqqqqqqqquuuuuuuuuuuuuuuuuuooooooooottttteeeeeeeeeeeeeeeeeeee';
+qp.innerText = choose(quotes);
